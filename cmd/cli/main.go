9b037203afb6f9cbd5d943ad1a353da2c52ab4cc
kahlys <youennlaborde@live.fr>
2021-01-30 23:14:41 +0100

    add configuration file to cli

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -5,32 +5,74 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-)
-
-var (
-	configFile string
-	protocol   string
-	address    []string
+	"github.com/spf13/viper"
 )
 
 func main() {
-	var rootCmd = &cobra.Command{
-		Short: "Transparence command linde client",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println(configFile, protocol, address)
-			panic("not implemented yet")
+	var (
+		configFile string
+		protocol   string
+		address    []string
+	)
+
+	rootCmd := &cobra.Command{
+		Short:         "Transparence command linde client",
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var config configValue
+
+			fmt.Println("READING CONFIGURATION")
+			viper.SetConfigFile(configFile)
+			if err := viper.ReadInConfig(); err != nil {
+				return fmt.Errorf("reading config file: %w", err)
+			}
+			if err := viper.Unmarshal(&config); err != nil {
+				return fmt.Errorf("unable to decode configuration: %w", err)
+			}
+
+			fmt.Println("parsed flags", configFile, protocol, address)
+			fmt.Println("configuration", protocol, address)
+			panic("not implemented")
 		},
 	}
 
 	rootCmd.PersistentFlags().StringVarP(&configFile, "config", "c", "", "configuration file")
 	rootCmd.PersistentFlags().StringVarP(&protocol, "protocol", "p", "", "protocol to explore ")
 	rootCmd.PersistentFlags().StringSliceVarP(&address, "address", "a", []string{}, "parameter for the function call")
-	rootCmd.MarkPersistentFlagRequired("config")
-	rootCmd.MarkPersistentFlagRequired("protocol")
-	rootCmd.MarkPersistentFlagRequired("address")
+	check(rootCmd.MarkPersistentFlagRequired("config"))
+	check(rootCmd.MarkPersistentFlagRequired("protocol"))
+	check(rootCmd.MarkPersistentFlagRequired("address"))
 
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	check(rootCmd.Execute())
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(0)
 	}
+}
+
+type configValue struct {
+	RPC         []RPC         `mapstructure:"rpc"`
+	Useraddress []UserAddress `mapstructure:"useraddress"`
+	Tokens      []Token       `mapstructure:"tokens"`
+}
+
+type RPC struct {
+	Blockchain string `mapstructure:"blockchain"`
+	Userpass   string `mapstructure:"userpass"`
+}
+
+type UserAddress struct {
+	Blockchain string `mapstructure:"blockchain"`
+	Address    string `mapstructure:"address"`
+}
+
+type Token struct {
+	BlockchainPlatform string   `mapstructure:"blockchain_platform"`
+	Symbol             string   `mapstructure:"symbol"`
+	ContractAddress    string   `mapstructure:"contract_address"`
+	BtcAddresses       []string `mapstructure:"btc_addresses"`
 }
