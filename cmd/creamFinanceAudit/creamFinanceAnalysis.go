d76bbb5e54acc16e6ddd62499f56c7e3ef632861
cryptomarabout <sami.barbara94fake@gmail.com>
2021-01-30 02:02:28 +0100

    feature/creamFinance_Binance: add compound analysis

--- cmd/creamFinanceAudit/creamFinanceAnalysis.go
+++ cmd/creamFinanceAudit/creamFinanceAnalysis.go
@@ -17,97 +17,112 @@ import (
      "transparence/pkg/comptroller"
 )
 
-// CreamFinance Ethereum requirementes
-const INFURA_KEY=""
+const ETH_STRING = "Ethereum"
+const BINANCE_STRING = "BinanceChain"
+
+var EXCLUDED_ADDRESSES = []string{"0xBdf447B39D152d6A234B4c02772B8ab5D1783F72","0xF5DCe57282A584D2746FaF1593d3121Fcac444dC"}
+
+// CreamFinance Ethereum requirements
+const INFURA_KEY="aefe8d98c6fd40adb30edab0e0954557"
 const IP_API_INFURA = "https://mainnet.infura.io/v3/" + INFURA_KEY
-const COMPTROLLER_CONTRACT_ADDRESS_ETHEREUM = "0x3d5BC3c8d13dcB8bF317092d84783c2697AE9258"
+const COMPTROLLER_CONTRACT_CREAM_ETHEREUM = "0x3d5BC3c8d13dcB8bF317092d84783c2697AE9258"
 const WETH_CONTRACT_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 
-// CreamFinance BinanceSmartChain requirementes
+// CreamFinance BinanceSmartChain requirements
 const IP_API_BINANCECHAIN = "wss://bsc-ws-node.nariox.org:443"
-const COMPTROLLER_CONTRACT_ADDRESS_BINANCE = "0x589de0f0ccf905477646599bb3e5c622c84cc0ba"
+const COMPTROLLER_CONTRACT_CREAM_BINANCE = "0x589de0f0ccf905477646599bb3e5c622c84cc0ba"
 const WBNB_CONTRACT_ADDRESS = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
 
-func main() {
-    fmt.Printf("\n\n ################## Analysis of Cream Finance on Ethereum ################## \n\n")
-    analyze(IP_API_INFURA,COMPTROLLER_CONTRACT_ADDRESS_ETHEREUM)
+// Compound Ethereum requirements
+const COMPTROLLER_CONTRACT_COMPOUND_ETHEREUM = "0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B"
 
-    fmt.Printf("\n\n ################## Analysis of Cream Finance on Binance ################## \n\n")
-    analyze(IP_API_BINANCECHAIN,COMPTROLLER_CONTRACT_ADDRESS_BINANCE)
+func main() {
+    analyze(IP_API_INFURA, COMPTROLLER_CONTRACT_CREAM_ETHEREUM, ETH_STRING)
+
+    analyze(IP_API_BINANCECHAIN, COMPTROLLER_CONTRACT_CREAM_BINANCE, BINANCE_STRING)
+
+    analyze(IP_API_INFURA, COMPTROLLER_CONTRACT_COMPOUND_ETHEREUM, ETH_STRING)
 }
 
-func analyze(ipAddress string, comptrollerAddress string){
-    var underlyingAddress common.Address
-
-    // Contract to exclude on creamFinance Ethereum
-    EXCLUDED_ADDRESSES := []string{"0xBdf447B39D152d6A234B4c02772B8ab5D1783F72"} 
-
+func analyze(ipAddress string, comptrollerAddress string, platform string){
 	client, err := ethclient.Dial(ipAddress)
     if err != nil {
         log.Fatal(err)
     }
-
-    markets := getAllCrTokensAddress(comptrollerAddress, client)
-
+    displayProtocolToken(comptrollerAddress,client,platform)
+    markets := getAllCTokensAddress(comptrollerAddress, client)
     for i := 1; i < len(markets); i++ {
         if !find(EXCLUDED_ADDRESSES, markets[i].String()) {
-            instance, err := crToken.NewCrToken(markets[i], client)
-            if err != nil {
-                log.Fatal(err)
-            }
-
-            symbol, name, decimals := getERC20InfosFromAddress(underlyingAddress, instance)
-
-            totalBorrows, err := instance.TotalBorrows(&bind.CallOpts{})
-            if err != nil {
-                log.Fatal(err)
-            }
-
-            totalSupply, err := instance.TotalSupply(&bind.CallOpts{})
-            if err != nil {
-                log.Fatal(err)
-            }
-
-            totalReserve, err := instance.TotalReserves(&bind.CallOpts{})
-            if err != nil {
-                log.Fatal(err)
-            }
-
-            cash, err := instance.GetCash(&bind.CallOpts{})
-            if err != nil {
-                log.Fatal(err)
-            }
-
-            if symbol == "crETH" {
-                underlyingAddress = common.HexToAddress(WETH_CONTRACT_ADDRESS)
-            }
-            if symbol == "crBNB" {
-                underlyingAddress = common.HexToAddress(WBNB_CONTRACT_ADDRESS)
-            } else {
-                underlyingAddress, err = instance.Underlying(&bind.CallOpts{})
-                if err != nil {
-                    log.Fatal(err)
-                }
-            }
-
-            underlyingInstance, err := crToken.NewCrToken(underlyingAddress, client)
-            if err != nil {
-                log.Fatal(err)
-            }
-
-            // Querying the uderlying token using erc20 abi (ex: query USDT contract for crUSDT)
-            underlyingSymbol, underlyingName, underlyingDecimals := getERC20InfosFromAddress(underlyingAddress, underlyingInstance)
-            
-            fmt.Printf("\n ---------------------")
-            fmt.Printf("\n - CreamToken Address: %s | Name: %s | Symbol: (%s)", markets[i], name, symbol)
-            fmt.Printf("\n - ERC20Token Address: %s | Name: %s | Symbol: (%s)", underlyingAddress, underlyingName, underlyingSymbol)
-            fmt.Printf("\n - CreamFinance stats : TotalBorrows: %2.f %s | Reserve: %2.f ",parseDecimals(totalBorrows,decimals), symbol,parseDecimals(totalReserve,decimals))
-            fmt.Printf("\n - Capitalization : TotalSupply:%2.f %s | Cash:%2.f %s ", parseDecimals(totalSupply,decimals), symbol, parseDecimals(cash,underlyingDecimals), underlyingSymbol)
-            fmt.Printf("\n ---------------------\n")
-            }
+            displayCompoundTokenInfos(markets[i],client,platform)
         }
+    }
+}
 
+func displayCompoundTokenInfos(address common.Address, client *ethclient.Client, platform string) {
+    var underlyingAddress common.Address
     
+    instance, err := crToken.NewCrToken(address, client)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    symbol, name, decimals := getERC20InfosFromAddress(underlyingAddress, instance)
+
+    totalBorrows, err := instance.TotalBorrows(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    totalSupply, err := instance.TotalSupply(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    totalReserve, err := instance.TotalReserves(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    cash, err := instance.GetCash(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    if (symbol == "crETH" && platform == "binance") || symbol == "cETH" {
+        underlyingAddress = common.HexToAddress(WETH_CONTRACT_ADDRESS)
+    } else if symbol == "crBNB" {
+        underlyingAddress = common.HexToAddress(WBNB_CONTRACT_ADDRESS)
+    } else {
+        underlyingAddress, err = instance.Underlying(&bind.CallOpts{})
+        if err != nil {
+            log.Fatal(err)
+        }
+    }
+
+    underlyingInstance, err := crToken.NewCrToken(underlyingAddress, client)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Querying the uderlying token using erc20 abi (ex: query USDT contract for crUSDT)
+    underlyingSymbol, underlyingName, underlyingDecimals := getERC20InfosFromAddress(underlyingAddress, underlyingInstance)
+    
+    fmt.Printf("\n ---------------------")
+    fmt.Printf("\n - CreamToken Address: %s | Name: %s | Symbol: (%s)", address, name, symbol)
+    fmt.Printf("\n - ERC20Token Address: %s | Name: %s | Symbol: (%s)", underlyingAddress, underlyingName, underlyingSymbol)
+    fmt.Printf("\n - CreamFinance stats : TotalBorrows: %2.f %s | Reserve: %2.f ",parseDecimals(totalBorrows,decimals), symbol,parseDecimals(totalReserve,decimals))
+    fmt.Printf("\n - Capitalization : TotalSupply:%2.f %s | Cash:%2.f %s ", parseDecimals(totalSupply,decimals), symbol, parseDecimals(cash,underlyingDecimals), underlyingSymbol)
+    fmt.Printf("\n ---------------------\n")
+}
+
+func displayProtocolToken (comptrollerAddress string,client *ethclient.Client, platform string) {
+    platformTokenAddress := getPlatformTokenAddress(comptrollerAddress, client)
+    instance, err := crToken.NewCrToken(platformTokenAddress, client)
+    if err != nil {
+        log.Fatal(err)
+    }
+    platformSymbol, platformName, _ := getERC20InfosFromAddress(platformTokenAddress, instance)
+    fmt.Printf("\n\n\n ==================| '%s' Protocol analysis | Symbol '%s' | Blockchain '%s' |==================\n", platformName, platformSymbol, platform)
 }
 
 
@@ -118,7 +133,7 @@ func parseDecimals(totalSupply *big.Int, decimals uint8) (*big.Float){
     return value
 }
 
-func getAllCrTokensAddress(comptrollerAddress string, client *ethclient.Client) ([]common.Address){
+func getAllCTokensAddress(comptrollerAddress string, client *ethclient.Client) ([]common.Address){
     tokenAddress := common.HexToAddress(comptrollerAddress)
     instance, err := comptroller.NewComptroller(tokenAddress, client)
     if err != nil {
@@ -129,6 +144,19 @@ func getAllCrTokensAddress(comptrollerAddress string, client *ethclient.Client) 
         log.Fatal(err)
     }
     return markets
+}
+
+func getPlatformTokenAddress(comptrollerAddress string, client *ethclient.Client) (common.Address){
+    tokenAddress := common.HexToAddress(comptrollerAddress)
+    instance, err := comptroller.NewComptroller(tokenAddress, client)
+    if err != nil {
+        log.Fatal(err)
+    }
+    platformTokenAddress, err := instance.GetCompAddress(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    return platformTokenAddress
 }
 
 
