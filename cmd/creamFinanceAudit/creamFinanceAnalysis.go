9ca154e9b841ecc8a78050c1c175146367e924b8
cryptomarabout <sami.barbara94fake@gmail.com>
2021-01-31 17:43:05 +0100

    refactoring: added cTokenAdapter pkg

--- cmd/creamFinanceAudit/creamFinanceAnalysis.go
+++ cmd/creamFinanceAudit/creamFinanceAnalysis.go
@@ -2,19 +2,13 @@ package main
 
 
 import (
-
-
     "fmt"
-    "log"
-    "math"
-    "math/big"
 
     "github.com/ethereum/go-ethereum/common"
-    "github.com/ethereum/go-ethereum/ethclient"  
-    "github.com/ethereum/go-ethereum/accounts/abi/bind" 
+    "github.com/ethereum/go-ethereum/ethclient"
 
-     "transparence/pkg/crToken"
-     "transparence/pkg/comptroller"
+    "transparence/pkg/cTokenAdapter"
+    "transparence/pkg/TransparenceUtils"
 )
 
 const ETH_STRING = "Ethereum"
@@ -45,14 +39,17 @@ func main() {
 }
 
 func analyze(ipAddress string, comptrollerAddress string, platform string){
-	client, err := ethclient.Dial(ipAddress)
-    if err != nil {
-        log.Fatal(err)
-    }
-    displayProtocolToken(comptrollerAddress,client,platform)
-    markets := getAllCTokensAddress(comptrollerAddress, client)
+    client := cTokenAdapter.NewClientConnection(ipAddress)
+    tokenAddress := common.HexToAddress(comptrollerAddress)
+    instanceComptroller := cTokenAdapter.NewComptroller(tokenAddress,client)     
+    platformTokenAddress :=cTokenAdapter.GetCompAddress(instanceComptroller)
+    instance := cTokenAdapter.NewCrToken(platformTokenAddress, client)
+    displayProtocolToken(cTokenAdapter.GetName(instance), cTokenAdapter.GetSymbol(instance), platform)
+
+    markets := cTokenAdapter.GetAllMarkets(instanceComptroller)
     for i := 1; i < len(markets); i++ {
-        if !find(EXCLUDED_ADDRESSES, markets[i].String()) {
+        _, isExcluded := transparenceutils.Find(EXCLUDED_ADDRESSES, markets[i].String())
+        if !isExcluded {
             displayCompoundTokenInfos(markets[i],client,platform)
         }
     }
@@ -60,128 +57,37 @@ func analyze(ipAddress string, comptrollerAddress string, platform string){
 
 func displayCompoundTokenInfos(address common.Address, client *ethclient.Client, platform string) {
     var underlyingAddress common.Address
-    
-    instance, err := crToken.NewCrToken(address, client)
-    if err != nil {
-        log.Fatal(err)
-    }
 
-    symbol, name, decimals := getERC20InfosFromAddress(underlyingAddress, instance)
-
-    totalBorrows, err := instance.TotalBorrows(&bind.CallOpts{})
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    totalSupply, err := instance.TotalSupply(&bind.CallOpts{})
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    totalReserve, err := instance.TotalReserves(&bind.CallOpts{})
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    cash, err := instance.GetCash(&bind.CallOpts{})
-    if err != nil {
-        log.Fatal(err)
-    }
+    instance := cTokenAdapter.NewCrToken(address, client)
+    name := cTokenAdapter.GetName(instance)
+    symbol := cTokenAdapter.GetSymbol(instance)
+    decimals := cTokenAdapter.GetDecimals(instance)
+    totalSupply := cTokenAdapter.GetTotalSupply(instance)
+    totalBorrows := cTokenAdapter.GetTotalBorrows(instance)
+    totalReserve := cTokenAdapter.GetTotalReserves(instance)
+    cash := cTokenAdapter.GetCash(instance)
 
     if (symbol == "crETH" && platform == "binance") || symbol == "cETH" {
         underlyingAddress = common.HexToAddress(WETH_CONTRACT_ADDRESS)
     } else if symbol == "crBNB" {
         underlyingAddress = common.HexToAddress(WBNB_CONTRACT_ADDRESS)
     } else {
-        underlyingAddress, err = instance.Underlying(&bind.CallOpts{})
-        if err != nil {
-            log.Fatal(err)
-        }
+        underlyingAddress = cTokenAdapter.GetUnderlying(instance)
     }
 
-    underlyingInstance, err := crToken.NewCrToken(underlyingAddress, client)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    // Querying the uderlying token using erc20 abi (ex: query USDT contract for crUSDT)
-    underlyingSymbol, underlyingName, underlyingDecimals := getERC20InfosFromAddress(underlyingAddress, underlyingInstance)
+    underlyingInstance := cTokenAdapter.NewCrToken(underlyingAddress, client)
+    underlyingName := cTokenAdapter.GetName(underlyingInstance)
+    underlyingSymbol := cTokenAdapter.GetSymbol(underlyingInstance)
+    underlyingDecimals := cTokenAdapter.GetDecimals(underlyingInstance)
     
     fmt.Printf("\n ---------------------")
     fmt.Printf("\n - CreamToken Address: %s | Name: %s | Symbol: (%s)", address, name, symbol)
     fmt.Printf("\n - ERC20Token Address: %s | Name: %s | Symbol: (%s)", underlyingAddress, underlyingName, underlyingSymbol)
-    fmt.Printf("\n - CreamFinance stats : TotalBorrows: %2.f %s | Reserve: %2.f ",parseDecimals(totalBorrows,decimals), symbol,parseDecimals(totalReserve,decimals))
-    fmt.Printf("\n - Capitalization : TotalSupply:%2.f %s | Cash:%2.f %s ", parseDecimals(totalSupply,decimals), symbol, parseDecimals(cash,underlyingDecimals), underlyingSymbol)
+    fmt.Printf("\n - CreamFinance stats : TotalBorrows: %2.f %s | Reserve: %2.f ",transparenceutils.ParseDecimals(totalBorrows,decimals), symbol,transparenceutils.ParseDecimals(totalReserve,decimals))
+    fmt.Printf("\n - Capitalization : TotalSupply:%2.f %s | Cash:%2.f %s ", transparenceutils.ParseDecimals(totalSupply,decimals), symbol, transparenceutils.ParseDecimals(cash,underlyingDecimals), underlyingSymbol)
     fmt.Printf("\n ---------------------\n")
 }
 
-func displayProtocolToken (comptrollerAddress string,client *ethclient.Client, platform string) {
-    platformTokenAddress := getPlatformTokenAddress(comptrollerAddress, client)
-    instance, err := crToken.NewCrToken(platformTokenAddress, client)
-    if err != nil {
-        log.Fatal(err)
-    }
-    platformSymbol, platformName, _ := getERC20InfosFromAddress(platformTokenAddress, instance)
+func displayProtocolToken (platformName string, platformSymbol string, platform string) {
     fmt.Printf("\n\n\n ==================| '%s' Protocol analysis | Symbol '%s' | Blockchain '%s' |==================\n", platformName, platformSymbol, platform)
-}
-
-
-func parseDecimals(totalSupply *big.Int, decimals uint8) (*big.Float){
-    tmp := new(big.Float)
-    tmp.SetString(totalSupply.String())
-    value := new(big.Float).Quo(tmp, big.NewFloat(math.Pow10(int(decimals))))
-    return value
-}
-
-func getAllCTokensAddress(comptrollerAddress string, client *ethclient.Client) ([]common.Address){
-    tokenAddress := common.HexToAddress(comptrollerAddress)
-    instance, err := comptroller.NewComptroller(tokenAddress, client)
-    if err != nil {
-        log.Fatal(err)
-    }
-    markets, err := instance.GetAllMarkets(&bind.CallOpts{})
-    if err != nil {
-        log.Fatal(err)
-    }
-    return markets
-}
-
-func getPlatformTokenAddress(comptrollerAddress string, client *ethclient.Client) (common.Address){
-    tokenAddress := common.HexToAddress(comptrollerAddress)
-    instance, err := comptroller.NewComptroller(tokenAddress, client)
-    if err != nil {
-        log.Fatal(err)
-    }
-    platformTokenAddress, err := instance.GetCompAddress(&bind.CallOpts{})
-    if err != nil {
-        log.Fatal(err)
-    }
-    return platformTokenAddress
-}
-
-
-func getERC20InfosFromAddress(address common.Address, instance *crToken.CrToken) (string, string, uint8) {
-    symbol, err := instance.Symbol(&bind.CallOpts{})
-    if err != nil {
-        log.Fatal(err)
-    }
-    decimals, err := instance.Decimals(&bind.CallOpts{})
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    name, err := instance.Name(&bind.CallOpts{})
-    if err != nil {
-        log.Fatal(err)
-    }
-    return symbol, name, decimals
-}
-
-func find(slice []string, val string) (bool) {
-    for _, item := range slice {
-        if item == val {
-            return true
-        }
-    }
-    return false
 }
---
+++ pkg/cTokenAdapter/cTokenAdapter.go
@@ -0,0 +1,125 @@
+package cTokenAdapter
+
+import (
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/ethclient"
+	
+     "transparence/pkg/crToken"
+     "transparence/pkg/comptroller"
+)
+
+func NewClientConnection (ipAddress string) (*ethclient.Client){
+	client, err := ethclient.Dial(ipAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
+
+func NewCrToken(tokenAddress common.Address, client *ethclient.Client) (*crToken.CrToken){
+	tokenInstance, err := crToken.NewCrToken(tokenAddress, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return tokenInstance
+}
+
+func NewComptroller(tokenAddress common.Address, client *ethclient.Client) (*comptroller.Comptroller){
+    instance, err := comptroller.NewComptroller(tokenAddress, client)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return instance
+}
+func GetName(cTokenInstance *crToken.CrToken) (string){
+	name, err := cTokenInstance.Name(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    return name
+}
+
+func GetSymbol(cTokenInstance *crToken.CrToken) (string){
+	symbol, err := cTokenInstance.Symbol(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    return symbol
+}
+
+func GetDecimals(cTokenInstance *crToken.CrToken) (uint8){
+	decimals, err := cTokenInstance.Decimals(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    return decimals
+}
+
+func GetTotalSupply(cTokenInstance *crToken.CrToken) (*big.Int){
+	totalSupply, err := cTokenInstance.TotalSupply(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    return totalSupply
+}
+
+func GetBalanceOfToken(addressToQuery string, cTokenInstance *crToken.CrToken) *big.Int {
+	address := common.HexToAddress(addressToQuery)
+	bal, err := cTokenInstance.BalanceOf(&bind.CallOpts{}, address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return bal
+}
+
+func GetTotalBorrows(cTokenInstance *crToken.CrToken) (*big.Int){
+	totalBorrows, err := cTokenInstance.TotalBorrows(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    return totalBorrows
+}
+
+func GetTotalReserves(cTokenInstance *crToken.CrToken) (*big.Int){
+	totalReserves, err := cTokenInstance.TotalReserves(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    return totalReserves
+}
+
+func GetCash(cTokenInstance *crToken.CrToken) (*big.Int){
+	cash, err := cTokenInstance.GetCash(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    return cash
+}
+
+func GetUnderlying(cTokenInstance *crToken.CrToken) (common.Address){
+	underlyingAddress, err := cTokenInstance.Underlying(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    return underlyingAddress
+}
+
+func GetCompAddress(currentComptroller *comptroller.Comptroller) (common.Address){
+	platformTokenAddress, err := currentComptroller.GetCompAddress(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    return platformTokenAddress
+}
+
+func GetAllMarkets(currentComptroller *comptroller.Comptroller) ([]common.Address){
+    markets, err := currentComptroller.GetAllMarkets(&bind.CallOpts{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    return markets
+}
