43da98a69e88a5637ce695ee98fc74616b05cdac
cryptohazard <jyezie@yahoo.fr>
2021-02-26 21:44:47 +0100

    first implem. check if value accurate

--- cmd/uma/main.go
+++ cmd/uma/main.go
@@ -2,14 +2,18 @@ package main
 import (
     "context"
     "fmt"
+    "sort"
     "log"
-    "math"
     "math/big"
     "flag"
 
     "github.com/ethereum/go-ethereum/common"
+    "github.com/superhawk610/bar"
+	  "github.com/ttacon/chalk"
     "transparence/pkg/blockchains/ethereum"
 )
+
+const start = 11766939
 func main() {
 	rpcURL := flag.String("r", "127.0.0.1:8545", "rpc url ")
 
@@ -21,24 +25,72 @@ func main() {
 	fmt.Println("Connected")
 
   account := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
-   balance, err := client.RPC.BalanceAt(context.Background(), account, nil)
-   if err != nil {
-       log.Fatal(err)
-   }
-   fmt.Println(balance) // 25893180161173005034
+  balance, err := client.RPC.BalanceAt(context.Background(), account, nil)
+  if err != nil {
+     log.Fatal(err)
+  }
+  fmt.Println(balance) // 25893180161173005034
 
-   blockNumber := big.NewInt(5532993)
-   balanceAt, err := client.RPC.BalanceAt(context.Background(), account, blockNumber)
-   if err != nil {
-       log.Fatal(err)
-   }
-   fmt.Println(balanceAt) // 25729324269165216042
+  blockStart := big.NewInt(start)
+  end,err:=client.RPC.BlockNumber(context.Background())
+  if err != nil {
+      log.Fatal(err)
+  }
+  blockEnd:=big.NewInt(int64(end))
+  b := bar.NewWithOpts(
+		bar.WithDimensions(int(end-start+1), 50),
+		bar.WithFormat(
+			fmt.Sprintf(
+				"   %sloading...%s :percent :bar %s:rate ops/s%s ",
+				chalk.Blue,
+				chalk.Reset,
+				chalk.Green,
+				chalk.Reset,
+			),
+		),
+	)
+  var TXes transactions
+  var totalGasUsed uint64
+  for i:=blockStart.Int64();i<=blockEnd.Int64();i++{
+    block,err:=client.RPC.BlockByNumber(context.Background(),big.NewInt(i))
+    if err != nil {
+        log.Fatal(err)
+    }
+    //fmt.Println(block)
+    //fmt.Println(block.Header())
+    b.Tick()
+    totalGasUsed+=block.GasUsed()
+    body:=block.Body()
+    for _,tx:=range body.Transactions{
+      TXes=append(TXes,transaction{tx.GasPrice(),tx.Gas()})
+    }
+  }
+  b.Done()
+  fmt.Println("Total Gas used ",totalGasUsed)
+  //fmt.Println(TXes)
+  sort.Slice(TXes, func(i, j int) bool {
+    return TXes[i].gasPrice.Cmp(TXes[j].gasPrice)<0
+  })
+  //fmt.Println(TXes)
+  med:=median(TXes,totalGasUsed)
+  //medianGwei:=med.Div()
+  fmt.Println("Median Gas Price: ",med)
 
-   fbalance := new(big.Float)
-   fbalance.SetString(balanceAt.String())
-   ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
-   fmt.Println(ethValue) // 25.729324269165216041
+}
 
-   pendingBalance, _ := client.RPC.PendingBalanceAt(context.Background(), account)
-   fmt.Println(pendingBalance) // 25729324269165216042
+type transaction struct{
+  gasPrice *big.Int
+  gasUsed  uint64
+}
+type transactions []transaction
+
+func median(sortedTxs transactions, totalGasUsed uint64) *big.Int{
+  var rollingSum uint64
+  for _,tx:= range sortedTxs{
+    rollingSum+=tx.gasUsed
+    if rollingSum>totalGasUsed/2{
+      return tx.gasPrice
+      }
+  }
+  return big.NewInt(0)
 }
--- go.mod
+++ go.mod
@@ -8,8 +8,13 @@ require (
 	github.com/ethereum/go-ethereum v1.9.25
 	github.com/fatih/color v1.10.0
 	github.com/go-chi/chi v1.5.1 // indirect
+	github.com/mattn/go-tty v0.0.3 // indirect
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.0
+	github.com/superhawk610/bar v0.0.2
+	github.com/superhawk610/terminal v0.1.0 // indirect
+	github.com/ttacon/chalk v0.0.0-20160626202418-22c06c80ed31
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	gonum.org/v1/gonum v0.8.2
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
